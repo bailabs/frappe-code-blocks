015c7ef3345e804ab284394d576d36439b47d42b
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-15 14:50:29 +0800

    added: date range filter

---
+++ reports/date_range_filter.js
@@ -0,0 +1,16 @@
+frappe.query_reports["Timesheet Summary"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		}
+	]
+}
